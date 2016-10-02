c151170def7cef55d0e1bb82ae7cb9004489105c
Jesse Barocio <jesse@barocio.net>
2016-10-02 16:39:14 -0600

    Added empty landing page.

---
+++ PeopleSearch/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PeopleSearch
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/libraries")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/angular.js")
+                .Include("~/Scripts/angular-route.js")
+                .Include("~/Scripts/angular-resource.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ PeopleSearch/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeopleSearch.Controllers
+{
+    [RoutePrefix("")]
+    public class HomeController : Controller
+    {
+        [HttpGet, Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
